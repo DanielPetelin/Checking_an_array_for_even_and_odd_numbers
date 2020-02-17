6ac682e4305549a29c13195a1162874686e90e05
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-02-17 21:58:26 +0300

    final commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-var a = 123;
+var a = [12, 43, 213, 75, 'eywr', 'dwaijira', 23, 48, 126, 743, 'programms', 178, 100, 90, 21, 80, 32, 1000];
+document.write(a);
 
-document.getElementById('chet').onclick = function(v) {
-    if (a % 2 == 0) {
-        document.getElementById('out').innerHTML = 'чётное';
-    } else {
-        document.getElementById('out').innerHTML = 'нечётное';
-    }
+function chetNumbers(array) {
+    var arrCh = [];
+    var arrNch = [];
+
+    for (var i in array) {
+        if (!isNaN(array[i])) {
+            if (array[i] % 2 == 0) {
+                arrNch.push(array[i]);
+            } else {
+                arrCh.push(array[i]);
+            };
+        };
+    };
+    return [arrNch, arrCh];
+};
+
+function innerHtmlNumbers() {
+    document.write(chetNumbers(a));
 };
